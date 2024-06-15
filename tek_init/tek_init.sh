36b9abc02eb53b6a06c0c23bc5b77b43aabf5cba
tomSeng <tom.seng@epitech.eu>
2024-06-15 23:04:59 +0200

    [UPDATE] updated tek_init to init coding styled files

--- tek_init/tek_init.sh
+++ tek_init/tek_init.sh
@@ -29,39 +29,44 @@ for file in $(find ./src -name "*.c"; echo "./includes/$root_dir.h"); do
     fi
     
     if [[ "$file" == */main.c ]]; then
-        echo -e "\nint main(void)\n{\n\treturn 0;\n}" >> "$file"
+        echo -e "\nint main(void)\n{\n    return 0;\n}" >> "$file"
     fi
 done
 
 # adding the ifndef to the .h file template
 echo -e "\n#ifndef ${root_dir^^}" >> "./includes/$root_dir.h"
-echo -e "#define ${root_dir^^}" >> "./includes/$root_dir.h"
-echo -e "\n\n#endif" >> "./includes/$root_dir.h"
+echo -e "    #define ${root_dir^^}" >> "./includes/$root_dir.h"
+echo -e "\n#endif" >> "./includes/$root_dir.h"
 
 # adding everything for the Makefile template
 ## defining variables
-echo -e "CC = gcc\nAR = rcs\n" > Makefile
+echo -e "##\n## EPITECH PROJECT, $year\n## $root_dir\n## File description:\n## Makefile\n##\n" > Makefile
+
+echo -e "CC = gcc\nAR = rcs\n" >> Makefile
 
 echo -e "TARGET = program\nTARGET_DEBUG = \$(TARGET)_debug" >> Makefile
-echo -e "CFLAGS_INCLUDES = -I\$(INCLUDE_DIR) \$(foreach dependencies,\$(DEPENDENCIES_PATH),-I\$(dependencies)/includes)" >> Makefile
+echo -e "CFLAGS_INCLUDES = -I\$(INCLUDE_DIR)\\" >> Makefile
+echo -e "\t\$(foreach dep,\$(DEPENDENCIES_PATH),-I\$(dep)/includes)" >> Makefile
 echo -e "CFLAGS = \$(CFLAGS_INCLUDES) -W -Wextra\nCFLAGS_DEBUG = -g\n" >> Makefile
 
-echo -e "INCLUDE_DIR = includes\nSRC_DIR = src\nOBJ_DIR = obj\nDEPENDENCIES_PATH = \n" >> Makefile
+echo -e "INCLUDE_DIR = includes\nSRC_DIR = src\nOBJ_DIR = obj\nDEPENDENCIES_PATH =\n" >> Makefile
 echo -e "SRC = \$(shell find \$(SRC_DIR) -name '*.c')\nOBJ = \$(patsubst \$(SRC_DIR)/%.c, \$(OBJ_DIR)/%.o, \$(SRC))\n" >> Makefile
 
 ## build
 echo -e "all: build_dependencies \$(TARGET)\n" >> Makefile
 
 echo -e "build_dependencies:" >> Makefile
-echo -e "\t@\$(foreach dependencies, \$(DEPENDENCIES_PATH), make -C \$(dependencies);)\n" >> Makefile
+echo -e "\t@\$(foreach dep, \$(DEPENDENCIES_PATH), make -C \$(dep);)\n" >> Makefile
 
 echo -e "\$(TARGET): \$(OBJ)" >> Makefile
-echo -e "\t\$(CC) \$(CFLAGS) \$(OBJ) \$(foreach dependencies,\$(DEPENDENCIES_PATH),-L\$(dependencies) -l\$(dependencies)) -o \$(TARGET)" >> Makefile
+echo -e "\t\$(CC) \$(CFLAGS) \$(OBJ)\\" >> Makefile
+echo -e "\t\$(foreach dep,\$(DEPENDENCIES_PATH),-L\$(dep) -l\$(dep)) -o \$(TARGET)" >> Makefile
 echo -e "\t@echo \"project '\$(TARGET)' successfully build\"\n" >> Makefile
 
 echo -e "\$(TARGET_DEBUG): \$(OBJ)" >> Makefile
 echo -e "\t@echo \"project '\$(TARGET_DEBUG)' successfully build\"" >> Makefile
-echo -e "\t\$(CC) \$(CFLAGS) \$(CFLAGS_DEBUG) \$(OBJ) \$(foreach dependencies,\$(DEPENDENCIES_PATH),-L\$(dependencies) -l\$(dependencies)) -o \$(TARGET_DEBUG)\n" >> Makefile
+echo -e "\t\$(CC) \$(CFLAGS) \$(CFLAGS_DEBUG) \$(OBJ)\\" >> Makefile
+echo -e "\t\$(foreach dep,\$(DEPENDENCIES_PATH),-L\$(dep) -l\$(dep)) -o \$(TARGET_DEBUG)\n" >> Makefile
 
 echo -e "\$(OBJ_DIR)/%.o: \$(SRC_DIR)/%.c | \$(OBJ_DIR)" >> Makefile
 echo -e "\t@mkdir -p \$(dir \$@)" >> Makefile
@@ -74,11 +79,11 @@ echo -e "\t@echo \"making directory '\$(OBJ_DIR)' since it doesn't exist\"\n" >>
 echo -e "debug: build_dependencies \$(TARGET_DEBUG)\n" >> Makefile
 
 echo -e "clean:" >> Makefile
-echo -e "\t@\$(foreach dependencies, \$(DEPENDENCIES_PATH), make -C \$(dependencies) clean;)" >> Makefile
+echo -e "\t@\$(foreach dep, \$(DEPENDENCIES_PATH), make -C \$(dep) clean;)" >> Makefile
 echo -e "\t@rm -rf \$(OBJ_DIR)\n" >> Makefile
 
 echo -e "fclean: clean" >> Makefile
-echo -e "\t@\$(foreach dependencies, \$(DEPENDENCIES_PATH), make -C \$(dependencies) fclean;)" >> Makefile
+echo -e "\t@\$(foreach dep, \$(DEPENDENCIES_PATH), make -C \$(dep) fclean;)" >> Makefile
 echo -e "\t@rm -f \$(TARGET)\n" >> Makefile
 
 echo -e "re: fclean all\n" >> Makefile
